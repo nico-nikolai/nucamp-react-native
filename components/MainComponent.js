5ef9a24a6a5b043358598d8b40b3d989c5ba386b
Nick Rose <nickfbp@gmail.com>
2020-11-25 16:32:24 -0700

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
+import Reservation from "./ReservationComponent";
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
@@ -125,6 +126,29 @@ const ContactNavigator = createStackNavigator(
   }
 );
 
+const ReservationNavigator = createStackNavigator(
+  {
+    Reservation: { screen: Reservation },
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+        headerStyle: {
+            backgroundColor: '#5637DD'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        },
+        headerLeft: <Icon
+            name='tree'
+            type='font-awesome'
+            iconStyle={styles.stackIcon}
+            onPress={() => navigation.toggleDrawer()}
+        />
+    })
+  }
+);
+
 const CustomDrawerContentComponenet = props => (
     <ScrollView>
         <SafeAreaView
@@ -175,6 +199,20 @@ const MainNavigator = createDrawerNavigator(
             )
         }
     },
+    Reservation: { 
+      screen: ReservationNavigator,
+      navigationOptions: {
+          drawerLabel: 'Reserve Campsite',
+          drawerIcon: ({tintColor}) => (
+              <Icon
+                  name='tree'
+                  type='font-awesome'
+                  size={24}
+                  color={tintColor}
+              />
+          )
+      }
+  },
     About: {
         screen: AboutNavigator,
         navigationOptions: {
